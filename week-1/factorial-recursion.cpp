c6c6d447e7f3796e5045c53067cda9545cce8689
Ahmed Baari <ahmed4baari@gmail.com>
2023-03-12 12:43:06 +0530

    indentation of lines

--- week-1/factorial-recursion.cpp
+++ week-1/factorial-recursion.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 /* Function to recursively multiply the number */
 long recursive_multiply(long number) {
-if (number != 0) 
-	return number * recursive_multiply(number - 1); // Eg: 5 * 4 * 3 * 2
-else 
-	return 1;
+	if (number != 0) 
+		return number * recursive_multiply(number - 1); // Eg: 5 * 4 * 3 * 2
+	else 
+		return 1;
 }
 
 /* Main Function */
 int main () {
-int factorial_of; // The user input will be stored here
+	int factorial_of; // The user input will be stored here
 
-/* Taking the number by input */
-cout << "Enter the number to find the factorial of: ";
-cin >> factorial_of;
+	/* Taking the number by input */
+	cout << "Enter the number to find the factorial of: ";
+	cin >> factorial_of;
 
-/* Calling the recursive multiplication function */
-long answer = recursive_multiply(factorial_of);
+	/* Calling the recursive multiplication function */
+	long answer = recursive_multiply(factorial_of);
 
-/* Printing the answer */
-cout << "The factorial of " << factorial_of << " is " << answer << endl;
+	/* Printing the answer */
+	cout << "The factorial of " << factorial_of << " is " << answer << endl;
 }
