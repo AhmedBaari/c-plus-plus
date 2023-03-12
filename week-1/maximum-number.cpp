33299bcdcce0b497ff6f73d9403b4fc0b927d0fc
Ahmed Baari <ahmed4baari@gmail.com>
2023-03-12 12:45:41 +0530

    indentation in maximum number

--- week-1/maximum-number.cpp
+++ week-1/maximum-number.cpp
@@ -5,24 +5,23 @@ using namespace std;
 
 /* Function to determine the maximum number among 3 numbers */
 inline int largest(int n1,int n2,int n3) { // Using inline function here
-int max = n1; // Assuming that n1 is the largest
-if (n2 > max) max = n2; 
-if (n3 > max) max = n3;
+        int max = n1; // Assuming that n1 is the largest
+        if (n2 > max) max = n2; 
+        if (n3 > max) max = n3;
 
-return max; // returning the max value
+        return max; // returning the max value
 }
 
 /* Main Function */
 int main() {
-int a,b,c;
+        int a,b,c;
 
-/* Collecting user input for 3 numbers */
-cout << "Enter 3 numbers seperated by space: ";
-cin >> a >> b >> c;
+        /* Collecting user input for 3 numbers */
+        cout << "Enter 3 numbers seperated by space: ";
+        cin >> a >> b >> c;
 
-/* Printing the largest of the 3 numbers by using largest() function */
-cout << "Maximum is " << largest(a,b,c) << "." << endl;
-
+        /* Printing the largest of the 3 numbers by using largest() function */
+        cout << "Maximum is " << largest(a,b,c) << "." << endl;
 }
 
 
