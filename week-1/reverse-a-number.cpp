21f56c3e0a44ff1b206e17ec8e166b31a546dcc5
Ahmed Baari <ahmed4baari@gmail.com>
2023-03-12 12:46:27 +0530

    indentation in reverse a number prog

--- week-1/reverse-a-number.cpp
+++ week-1/reverse-a-number.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main () {
-long initialNumber, finalNumber;
+	long initialNumber, finalNumber;
 
-/* Taking user input for initial number */
-cout << "Enter a number to reverse: ";
-cin >> initialNumber;
+	/* Taking user input for initial number */
+	cout << "Enter a number to reverse: ";
+	cin >> initialNumber;
 
-int n=1, remainder;
+	int n=1, remainder;
 
-/* While loop to loop through each digit and thus reverse the number */
-while (initialNumber!=0) { 			
-	remainder = initialNumber % 10; //The value in the last digit
-	finalNumber = finalNumber * 10 + remainder; //Appending it to final number
-	initialNumber = initialNumber / 10; //Removing last digit in the initial number
-}
+	/* While loop to loop through each digit and thus reverse the number */
+	while (initialNumber!=0) { 			
+		remainder = initialNumber % 10; //The value in the last digit
+		finalNumber = finalNumber * 10 + remainder; //Appending it to final number
+		initialNumber = initialNumber / 10; //Removing last digit in the initial number
+	}
 
-/* Printing the reversed number */
-cout << finalNumber;
+	/* Printing the reversed number */
+	cout << finalNumber;
 
 }
