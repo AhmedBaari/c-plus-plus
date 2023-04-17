4852eb0138de129dcec3a55028c8d36fa224aa08
Ahmed Baari <ahmed4baari@gmail.com>
2023-04-17 09:04:38 +0530

    Rectified mistakes in fruit color search

--- week-6/fruit-color-search.cpp
+++ week-6/fruit-color-search.cpp
@@ -1,53 +1,64 @@
 /* In this program, we will enter fruit details search for fruits by their color */
 #include <iostream>
+#include <string>
 using namespace std;
 
-class fruit {
+class fruit
+{
 	string shape, taste;
 	float rate;
-	
-public: 
+
+public:
 	string color, name;
-	
-	void getData () {
+
+	void getData()
+	{
 		cout << "Enter color, name, shape, taste: ";
 		cin >> color >> name >> shape >> taste;
 	}
-	
-	void putData () {
-		cout << "name: " name << "\nshape: "shape << "\ntaste: " taste << "\nrate: " << rate << endl;
+
+	void putData()
+	{
+		cout << "name: " << name << "\nshape: " << shape << "\ntaste: " << taste << "\nrate: " << rate << endl;
 	}
-	
-	void getRate() {
+
+	void getRate()
+	{
 		cout << "Enter Rate: ";
 		cin >> rate;
 	}
-	
-	string return_color() {
+
+	string return_color()
+	{
 		return color;
 	}
 };
 
-main () {
+main()
+{
 	fruit f[3];
 	int i;
 	string search;
-	
-	for (i=0; i<3; i++) {
+
+	for (i = 0; i < 3; i++)
+	{
 		f[i].getData();
 		f[i].getRate();
 	}
-	
+
 	cout << "Enter Color: ";
 	cin >> search;
-	
-	for(i=0; i<3; i++) {
+
+	for (i = 0; i < 3; i++)
+	{
 		int success = 0;
-		if (search == f[i].return_color()) {
+		if (search == f[i].return_color())
+		{
 			f[i].putData();
 			success = 1;
-		} 
-		if (success == 0) {
+		}
+		if (success == 0)
+		{
 			cout << "no match";
 		}
 	}
